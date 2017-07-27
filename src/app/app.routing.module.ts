a359ae02e3ecb3177f730e9351f26b2ccb965dbb
Heltonss <helton@heltonsouza.com.br>
2017-07-26 21:54:41 -0300

    created module main

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,21 +2,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './main/login/login.component';
 
 const appRoutes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
     component: LoginComponent
   }
-]
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
   declarations: [],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
